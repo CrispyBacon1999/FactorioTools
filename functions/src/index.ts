e307ca8318152e49ec78fab0be284690aef27a45
Josh Bacon <josh.bacon@nexteer.com>
2021-05-25 11:05:20 -0400

    See where the error is coming from

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,6 +10,7 @@ import { parseLuaFileData } from "./factorio/parse";
 // });
 
 export const parseFile = functions.https.onRequest((request, response) => {
+  response.status(200).send();
   const modid = request.query.modid;
   const type = request.query.type;
   const fileData = request.body.text;
